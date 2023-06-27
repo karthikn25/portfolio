4bc7458a35b09118bc56e6a72bc78f1a31fc1f77
karthikn25 <mkarthikeyan179@gmail.com>
2023-06-27 17:34:54 +0530

    color

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -7,11 +7,11 @@ export default function Topbar() {
     <header>
     <div className='logo'><span>K</span>arthiKn.</div>
     <ul className='navlist'>
-    <li><div className='a'><Link to="/" style={{color:"white"}}>Home</Link></div></li>
-    <li><div className='a' ><Link to="/about" style={{color:"white"}}>About</Link></div></li>
-    <li><div className='a'><Link to="/Skills" style={{color:"white"}}>Skills</Link></div></li>
-    <li><div className='a'><Link to="/work" style={{color:"white"}}>Work</Link></div></li>
-    <li><div className='a'><Link to="/contact" style={{color:"white"}}>Contact</Link></div></li>
+    <li><div className='a'><Link to="/" style={{color:"#12f7ff"}}>Home</Link></div></li>
+    <li><div className='a' ><Link to="/about" style={{color:"#12f7ff"}}>About</Link></div></li>
+    <li><div className='a'><Link to="/Skills" style={{color:"#12f7ff"}}>Skills</Link></div></li>
+    <li><div className='a'><Link to="/work" style={{color:"#12f7ff"}}>Work</Link></div></li>
+    <li><div className='a'><Link to="/contact" style={{color:"#12f7ff"}}>Contact</Link></div></li>
     </ul>
     <div id='menu-icon' className='bx bxl-github'></div>
     
